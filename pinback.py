cc6da9b23bb83670fec54619063675160a1c05be
Casey Boettcher <chb@socketbox.com>
2020-08-29 12:15:22 -0400

    Adding tests for functions related to recent inclusion of argparse and configparse

--- pinback.py
+++ pinback.py
@@ -1,22 +1,26 @@
-from datetime import date, datetime
+import configparser
 import time
 import re
 import requests
-import json
-import logging 
+import logging
 import sys
+import argparse
+from pathlib import Path
 import os
 
 RL_EP="https://robustlinks.mementoweb.org/api/?url={0}"
-ARCHIVE_DOMAIN="archive.org"
 PB_EP="https://api.pinboard.in/v1/posts/add?auth_token={0}&url={1}&description={2}&tags={3}&shared='no'"
+CFG_SECTION='DEFAULT'
 TOKEN_STR='PINBOARD_API_TOKEN'
+DEF_CFG_PATH='/.config/socketbox/pinback/pinback.cfg'
 
 '''
-check that PINBOARD_API_TOKEN is set
-get url and, optionally, archive from user
-append URL to Robust Links API URL
-make the call using requests
+Pseudocode overview:
+1. check that PINBOARD_API_TOKEN is set, either in the env, or in
+    ~/.config/socketbox/pinback.cfg
+2. get url and, optionally, archive from user
+3. append URL to Robust Links API URL
+4. make the call using requests
 check the response for status
 if 2xx
     if json has no errors, 
@@ -33,52 +37,65 @@ if other ie redirect
 '''
 
 
+def get_robust_resp(site_url: str) -> requests.models.Response:
+    """
 
-"""def get_formatted_today() -> date:
-    return date.today().strftime('%Y%m%d')
-"""
-
-
-
-def get_robust_link(site_url:str) -> requests.models.Response:
+    :param site_url:
+    :return:
+    """
     robust_url = RL_EP.format(site_url)
-    logging.debug("Getting link from Robust Link using {}".format(robust_url))
-    resp = requests.models.Response()
-    sleep_t = 10 
-    attempts = 0
-    while resp.status_code != 200 and attempts < 11:
-        print("Retrieving a link from Robust Link service...This could take quite some time.")
-        attempts += 1 
-        resp = requests.get(robust_url)
-        # this is not unusual; the Archive is very slow, so we wait and retry
-        if resp.status_code == 504 or resp.status_code == 502:
-            logging.warning("Gateway issue. Retrying after {}".format(sleep_t)) 
-            time.sleep(sleep_t)
-            sleep_t *= 1.5
-        # if status is in 400 range, recovery unlikely
-        elif resp.status_code >= 400 and resp.status_code < 500:
-            logging.warn(f"Response status {resp.status_code} unexpected.")
-            break
+    logging.info("Getting link from Robust Link using {}".format(robust_url))
+    resp = requests.get(robust_url)
     return resp
 
 
-"""
-    @
-"""
-def pin_url(url:str, tags:list, desc:str, token:str) -> requests.models.Response:
-    pinboard_url = PB_EP.format(token, url, desc, tags) 
-    logging.info("Pinning {}...".format(pinboard_url))
+def parse_robust_response( resp: requests.models.Response) -> tuple:
+    """
+
+    :param resp:
+    :return:
+    """
+    if resp.status_code == 200:
+        robust_data = resp.json()
+        res_tuple = (200, robust_data)
+    else:
+        res_tuple = (resp.status_code, None)
+    return res_tuple
+
+
+    """if  resp.status_code == 504 or resp.status_code == 502:
+        logging.warning(f"Gateway issue. Retrying after {sleep_t}")
+        time.sleep(sleep_t)
+        sleep_t *= 1.5
+    # if status is in 400 range, recovery unlikely
+    elif 400 <= resp.status_code < 500:
+        logging.warning(f"Response status {resp.status_code} unexpected.")
+        break
+    return resp
+    """
+
+
+
+def pin_url(url: str, tags: list, desc: str, token: str, share: bool) -> requests.models.Response:
+    """
+
+    :param share:
+    :param desc:
+    :param tags:
+    :param url:
+    :type token: object
+    """
+    pinboard_url = PB_EP.format(token, url, desc, tags, share)
+    logging.info(f"Pinning {pinboard_url}...")
     pb_resp = requests.get(pinboard_url) 
     return pb_resp 
 
 
-"""
-"""
 def prompt_for_description():
     desc = None 
     while not desc or len(desc) > 254:
         desc = input("Provide a description of less than 256 characters for the pin/bookmark: ").strip()
-    logging.debug("User-supplied description: {}".format(desc))
+    logging.debug(f"User-supplied description: {desc}")
     return desc
 
 
@@ -87,28 +104,89 @@ def prompt_for_tags():
     while not tags_str:
         tags_str = input("Provide a comma-delimited list of tags: ") 
     else:
-        logging.debug("User-supplied tag string: {}".format(tags_str))
+        logging.debug(f"User-supplied tag string: {tags_str}")
         patt = re.compile(r"^.*\s.*$")
         # check for bad chars (whitespace)
         tags = tags_str.split(',')
-        logging.debug("Tag list: {}".format(tags))
+        logging.debug(f"Tag list: {tags}")
         # return only tags matching the regex above 
         valid = [t for t in tags if re.match(patt, t)]
     return valid
 
 
-def main():
-    url = sys.argv[1] #TODO check_args()
-    token = os.environ[TOKEN_STR]
+def parse_args() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(description='Save an archived URL to Pinboard', prog='pinback',
+                                     usage='%(prog)s [options] url')
+    parser.add_argument('url', metavar='url', type=str, help='the original URL' +
+                                     '(non-archival URL) you intend to save to pinboard',
+                                    action='store')
+    parser.add_argument('-v', '--verbose', action='count', default=0)
+    parser.add_argument('--version', action='version', version='%(prog)s 1.0')
+    parser.add_argument('-k', '--token', action='store', help="A pinboard API token")
+    parser.add_argument('-t', '--tags', action='extend', nargs='+', type=str,
+                        help='A space-delimited list of tags for Pinboard')
+    parser.add_argument('-d', '--desc', action='store', type=str,
+                        help='A description (title) for the Pinboard entry')
+    parser.add_argument('-c', '--config', action='store',
+                default=Path.home() + '/.config/socketbox/pinback/pinback.cfg')
+    return parser
+
+
+def parse_config(cfg_file: str) -> dict:
+    """
+
+    :param cfg_file:
+    :return:
+    """
+    cfgp = configparser.ConfigParser()
+    if not cfg_file:
+        cfgp.read(cfg_file)
+    else:
+        home = str(Path.home())
+        cfgp.read(home + DEF_CFG_PATH)
+    return cfgp
+
+
+def  check_prereqs(argp: argparse.ArgumentParser, cfgp: configparser.ConfigParser):
+    """
+
+    :param argp:
+    :param cfgp:
+    :return:
+    """
+    if TOKEN_STR not in os.environ:
+        token = argp.token
     if not token:
-        print("Please provide a Pinboard API token. Currently that means setting {} in the environment".format(TOKEN_STR))
-        exit(1) 
-    # append the supplied date and archive to the Memento API endpoint location
-    resp = get_robust_link(url)
-    desc = prompt_for_description()
-    tags = prompt_for_tags()
-    pin_url(resp, tags, desc, token)
+        token = cfgp[CFG_SECTION][TOKEN_STR]
+    if not token:
+        raise ValueError("No Pinboard API token supplied. Exiting.")
+    return token
+
+
+def main():
+    arg_ns = parse_args()
+    cfg_parser = parse_config(arg_ns.config)
+    try:
+        token = check_prereqs(cfg_parser, arg_ns)
+    except ValueError as ve:
+        logging.error(ve)
+        sys.exit(1)
+    if arg_ns.verbose == 1:
+        logging.basicConfig(level=logging.INFO, stream=sys.stdout, force=True)
+    elif arg_ns.verbose > 1:
+        logging.basicConfig(level=logging.DEBUG, stream=sys.stdout, force=True)
     
+    resp = get_robust_resp(arg_ns.url)
+    rob_url = parse_robust_response(resp)
+    if desc := arg_ns.desc is None:
+        desc = prompt_for_description()
+    if tags := arg_ns.tags is None:
+        tags = prompt_for_tags()
+    if not tags or not desc:
+        logging.warning("Pinning without ")
+    pin_url(rob_url, tags, desc, token)
+
+
 if __name__ == '__main__':
     main()
 
--- test_pinback.py
+++ test_pinback.py
@@ -1,8 +1,8 @@
 import logging
 import os
 import string
-import time 
-import random 
+import time
+import random
 import pytest
 import json
 from io import StringIO
@@ -17,44 +17,54 @@ def test_get_formatted_today():
 """
 
 
-def test_get_robust_link():
+def test_get_robust_resp():
     url = "https://peacesupplies.org"
-    resp = pinback.get_robust_link(url) 
-    assert(resp.status_code == 200)
+    resp = pinback.get_robust_resp(url)
+    assert (resp.status_code == 200)
 
-def test_mementoize_url():
+
+def test_parse_args():
+    args = "-vv -k foo:12345678ABC -t my little lamb goes to the market -d My lamb doesn't rhyme"\
+           ""
+
+
+
+
+'''def test_mementoize_url():
     url = "https://hackaday.com/2020/07/16/tend-your-garden-again/"
     resp = pinback.mementoize_url(url)
-    assert resp.status_code == 200
+    assert resp.status_code == 200'''
+
 
 def test_prompt_for_tags(monkeypatch):
     monkeypatch.setattr('sys.stdin', StringIO('tag1,groovy,this fails,asdoesthis ,'))
     tags = pinback.prompt_for_tags()
-    assert(len(tags) == 2)
+    assert (len(tags) == 2)
 
 
 def test_prompt_for_desc_toolong(monkeypatch):
     with pytest.raises(EOFError, match=r'EOF when reading a line'):
-        monkeypatch.setattr('sys.stdin', StringIO('  this si a a tset alf alsdf fasldfkja aldjfaslkdfj a;sdlfkjas;dlkfja ;sdf aslkfdj a;sdf asldfkj a;sdkfj ;alskdjf ;aslkdjf ;alskjfd ;laskdjf ;lkasjdf;lkasjdflkjas ldkfjas;lkfjd;lsadkj flaskdjf lkasdjfsfdsfd adfasdf asdf      \rThis is my not so trim description'))
+        monkeypatch.setattr('sys.stdin', StringIO(
+            '  this si a a tset alf alsdf fasldfkja aldjfaslkdfj a;sdlfkjas;dlkfja ;sdf aslkfdj a;sdf asldfkj a;sdkfj ;alskdjf ;aslkdjf ;alskjfd ;laskdjf ;lkasjdf;lkasjdflkjas ldkfjas;lkfjd;lsadkj flaskdjf lkasdjfsfdsfd adfasdf asdf      \rThis is my not so trim description'))
         desc = pinback.prompt_for_description()
 
 
-def test_prompt_for_title(monkeypatch):
-    monkeypatch.setattr('sys.stdin',
-            StringIO('     This is my not so trim description     '))
-    title = pinback.prompt_for_description()
-    assert(title == 'This is my not so trim description')
+def test_prompt_for_desc(monkeypatch):
+    monkeypatch.setattr('sys.stdin', StringIO('Job description for Java Developer at Foobar'))
+    desc = pinback.prompt_for_description()
+    assert (desc == 'Job description for Java Developer at Foobar')
 
 
 def test_pin_url():
     token = os.environ[pinback.TOKEN_STR]
-    #https://stackoverflow.com/a/2257449/148680 
-    rand_dom = ''.join(random.choices(string.ascii_lowercase, k=1)).join(random.choices(string.ascii_uppercase + string.digits, k=4))
-    url = 'https://www.' + rand_dom + '.movie' 
-    tags = ["foo", "bar", "baz"] 
+    # https://stackoverflow.com/a/2257449/148680
+    rand_dom = ''.join(random.choices(string.ascii_lowercase, k=1)).join(
+        random.choices(string.ascii_uppercase + string.digits, k=4))
+    shared = False
+    url = 'https://www.' + rand_dom + '.movie'
+    tags = ["foo", "bar", "baz"]
     desc = "Test description."
-    logging.debug("Calling pin_url with: {}, {}, {}".format(url, tags, desc)) 
-    resp = pinback.pin_url(url, tags, desc, token) 
+    logging.debug("Calling pin_url with: {}, {}, {}, {}".format(url, tags, desc, shared))
+    resp = pinback.pin_url(url, tags, desc, token)
     logging.debug("pinboard response: {}".format(resp.text))
-    assert(resp !=  None and resp.status_code == 200)
-
+    assert (resp != None and resp.status_code == 200)
